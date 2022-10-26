eccc147c9168223a0ac9ae6425c81c9856fd1d87
sami081 <75616920+sami081@users.noreply.github.com>
2022-10-26 19:06:27 +0200

    page home

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const menu = document.getElementById("menu");
 const banner = document.querySelector(".banner");
 const divMenu = document.querySelector(".div-menu");
-
+const title = document.querySelector(".div-title");
+console.log(title);
 
 menu.addEventListener("click", () => {
   console.log("oui");
-//  divMenu.classList.remove("before")
- divMenu.classList.toggle("after")
+  //  divMenu.classList.remove("before")
+  divMenu.classList.toggle("after");
+  title.classList.toggle("after-title");
 });
